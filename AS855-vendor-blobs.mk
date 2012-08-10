d2b1ce44bc426d259aac42523e9298621d97d7dc
Tom Marshall <tdm.code@gmail.com>
2012-08-10 13:59:42 -0500

    Initial commit.

---
+++ AS855-vendor-blobs.mk
@@ -0,0 +1,2 @@
+PRODUCT_COPY_FILES += \
+	vendor/lge/AS855/proprietary/usr/keylayout/hub_synaptics_touch.kl:system/usr/keylayout/hub_synaptics_touch.kl
---
+++ AS855-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/lge/AS855/AS855-vendor-blobs.mk)
